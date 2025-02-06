2b3933c8cb9bd47b5cf51a6742c8263b4e719b07
viklop <viklop7025@gmail.com>
2025-02-06 18:40:23 -0500

    feat: validar formulario de la página básicos

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
+const product = {
+    name: 'RTX 5990',
+    price: 2570,
+    inStorage: 5,
+}
 @Component({
-  selector: 'app-basic-page',
-  standalone: false,
-  
-  templateUrl: './basic-page.component.html',
-  styleUrl: './basic-page.component.css'
-})
-export class BasicPageComponent {
+    selector: 'app-basic-page',
+    standalone: false,
 
+    templateUrl: './basic-page.component.html',
+    styleUrl: './basic-page.component.css',
+})
+export class BasicPageComponent implements OnInit {
+    private _fb: FormBuilder = new FormBuilder()
+
+    public myForm: FormGroup = this._fb.group({
+        name: ['', [Validators.required, Validators.minLength(3)]],
+        price: [0, [Validators.required, Validators.min(0)]],
+        inStorage: [0, [Validators.required, Validators.min(0)]],
+    })
+
+    ngOnInit(): void {
+        // this.myForm.reset(product)
+    }
+
+    isInvalidField(field: string): boolean | null {
+        return (
+            this.myForm.controls[field].errors &&
+            this.myForm.controls[field].touched
+        )
+    }
+
+    getFieldError(field: string): string | null {
+        if (!this.myForm.controls[field]) return null
+        const errors = this.myForm.controls[field].errors || {}
+
+        for (const key of Object.keys(errors)) {
+            switch (key) {
+                case 'required':
+                    return 'Este campo es requerido.'
+
+                case 'minlength':
+                    return `Mínimo ${errors['minlength'].requiredLength} caracteres.`
+            }
+        }
+
+        return null
+    }
+
+    onSave(): void {
+        if (this.myForm.invalid) {
+            this.myForm.markAllAsTouched()
+            return
+        }
+        this.myForm.reset({ price: 0, inStorage: 0 })
+    }
 }
--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -1,21 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { ReactiveFormsModule } from '@angular/forms'
 
-import { ReactiveRoutingModule } from './reactive-routing.module';
-import { BasicPageComponent } from './pages/basic-page/basic-page.component';
-import { DinamycPageComponent } from './pages/dinamyc-page/dinamyc-page.component';
-import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
-
+import { ReactiveRoutingModule } from './reactive-routing.module'
+import { BasicPageComponent } from './pages/basic-page/basic-page.component'
+import { DinamycPageComponent } from './pages/dinamyc-page/dinamyc-page.component'
+import { SwitchesPageComponent } from './pages/switches-page/switches-page.component'
 
 @NgModule({
-  declarations: [
-    BasicPageComponent,
-    DinamycPageComponent,
-    SwitchesPageComponent
-  ],
-  imports: [
-    CommonModule,
-    ReactiveRoutingModule
-  ]
+    declarations: [
+        BasicPageComponent,
+        DinamycPageComponent,
+        SwitchesPageComponent,
+    ],
+    imports: [CommonModule, ReactiveRoutingModule, ReactiveFormsModule],
 })
-export class ReactiveModule { }
+export class ReactiveModule {}
