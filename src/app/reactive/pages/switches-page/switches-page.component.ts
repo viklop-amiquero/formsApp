db9e808f632aa2028b7a069ce5ad645ccbbf9524
viklop <viklop7025@gmail.com>
2025-02-07 17:56:26 -0500

    feat: validar formulario de switches-page

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
-  selector: 'app-switches-page',
-  standalone: false,
-  
-  templateUrl: './switches-page.component.html',
-  styleUrl: './switches-page.component.css'
-})
-export class SwitchesPageComponent {
+    selector: 'app-switches-page',
+    standalone: false,
 
+    templateUrl: './switches-page.component.html',
+    styleUrl: './switches-page.component.css',
+})
+export class SwitchesPageComponent implements OnInit {
+    private _fb = new FormBuilder()
+
+    public person = {
+        gender: 'F',
+        wantNotifications: false,
+    }
+
+    ngOnInit(): void {
+        this.myForm.reset(this.person)
+    }
+
+    public myForm: FormGroup = this._fb.group({
+        gender: ['M', Validators.required],
+        wantNotifications: [true, Validators.required],
+        termsAndConditions: [false, Validators.requiredTrue],
+    })
+
+    isInvalidField(field: string): boolean | null {
+        return (
+            this.myForm.controls[field].errors &&
+            this.myForm.controls[field].touched
+        )
+    }
+
+    onSubmit(): void {
+        if (this.myForm.invalid) {
+            this.myForm.markAllAsTouched()
+            return
+        }
+
+        const { termsAndConditions, ...newPerson } = this.myForm.value
+
+        this.person = newPerson
+        console.log(this.person)
+    }
 }
