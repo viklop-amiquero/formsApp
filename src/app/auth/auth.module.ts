d8ed242aa58aab5514d34d9f938e644262644c28
viklop <viklop7025@gmail.com>
2025-02-08 11:56:53 -0500

    feat: validar formulario de register-page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,17 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
-import { AuthRoutingModule } from './auth-routing.module';
-import { RegisterPageComponent } from './pages/register-page/register-page.component';
-
+import { AuthRoutingModule } from './auth-routing.module'
+import { RegisterPageComponent } from './pages/register-page/register-page.component'
+import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
-  declarations: [
-    RegisterPageComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+    declarations: [RegisterPageComponent],
+    imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import * as customValidators from '../../../shared/validators/validators'
 
 @Component({
-  selector: 'app-register-page',
-  standalone: false,
-  
-  templateUrl: './register-page.component.html',
-  styleUrl: './register-page.component.css'
+    selector: 'app-register-page',
+    standalone: false,
+
+    templateUrl: './register-page.component.html',
+    styleUrl: './register-page.component.css',
 })
 export class RegisterPageComponent {
+    private _fb = new FormBuilder()
 
+    public myForm: FormGroup = this._fb.group({
+        name: [
+            '',
+            [
+                Validators.required,
+                Validators.pattern(
+                    customValidators.firstNameAndLastnamePattern
+                ),
+            ],
+        ],
+        email: [
+            '',
+            [
+                Validators.required,
+                Validators.pattern(customValidators.emailPattern),
+            ],
+        ],
+        username: ['', [Validators.required, customValidators.cantBeStrider]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        password2: ['', [Validators.required]],
+    })
+
+    isValidField(field: string) {
+        //todo obtener validación desde un servicio
+    }
+
+    onSubmit(): void {
+        if (this.myForm.invalid) {
+            this.myForm.markAllAsTouched()
+            return
+        }
+    }
 }
