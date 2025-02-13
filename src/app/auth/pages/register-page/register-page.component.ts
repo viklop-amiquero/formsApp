704647f0ed4c489f3ee19bc726a8dd161afbdeeb
viklop <viklop7025@gmail.com>
2025-02-13 10:00:35 -0500

    validacion de contraseñas iguales

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
-import * as customValidators from '../../../shared/validators/validators'
+// import * as customValidators from '../../../shared/validators/validators'
+import { ValidatorsService } from '../../../shared/services/validators.service'
+import { EmailValidator } from '../../../shared/validators/email-validators.service'
 
 @Component({
     selector: 'app-register-page',
@@ -11,34 +13,61 @@ import * as customValidators from '../../../shared/validators/validators'
 })
 export class RegisterPageComponent {
     private _fb = new FormBuilder()
+    public myForm!: FormGroup
 
-    public myForm: FormGroup = this._fb.group({
-        name: [
-            '',
-            [
-                Validators.required,
-                Validators.pattern(
-                    customValidators.firstNameAndLastnamePattern
-                ),
-            ],
-        ],
-        email: [
-            '',
-            [
-                Validators.required,
-                Validators.pattern(customValidators.emailPattern),
-            ],
-        ],
-        username: ['', [Validators.required, customValidators.cantBeStrider]],
-        password: ['', [Validators.required, Validators.minLength(6)]],
-        password2: ['', [Validators.required]],
-    })
+    constructor(
+        private _validatorService: ValidatorsService,
+        private _emailValidator: EmailValidator
+    ) {
+        this.createForm()
+    }
 
-    isValidField(field: string) {
+    createForm(): void {
+        this.myForm = this._fb.group(
+            {
+                name: [
+                    '',
+                    [
+                        Validators.required,
+                        Validators.pattern(
+                            this._validatorService.firstNameAndLastnamePattern
+                        ),
+                    ],
+                ],
+
+                email: [
+                    '',
+                    [
+                        Validators.required,
+                        Validators.pattern(this._validatorService.emailPattern),
+                    ],
+                    [this._emailValidator],
+                ],
+                username: [
+                    '',
+                    [Validators.required, this._validatorService.cantBeStrider],
+                ],
+                password: ['', [Validators.required, Validators.minLength(6)]],
+                password2: ['', [Validators.required]],
+            },
+            {
+                validators: [
+                    this._validatorService.isFieldOneEqualFieldTwo(
+                        'password',
+                        'password2'
+                    ),
+                ],
+            }
+        )
+    }
+
+    isInvalidField(field: string) {
         //todo obtener validación desde un servicio
+        return this._validatorService.isInvalidField(this.myForm, field)
     }
 
     onSubmit(): void {
+        console.log(this.myForm.value)
         if (this.myForm.invalid) {
             this.myForm.markAllAsTouched()
             return
---
+++ src/app/shared/services/validators.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core'
+import {
+    AbstractControl,
+    FormControl,
+    FormGroup,
+    ValidationErrors,
+} from '@angular/forms'
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ValidatorsService {
+    public firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)'
+    public emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'
+
+    cantBeStrider = (control: FormControl): ValidationErrors | null => {
+        const value: string = control.value.trim().toLowerCase()
+
+        if (value === 'strider') {
+            return {
+                noStrider: true,
+            }
+        }
+
+        return null
+    }
+
+    isInvalidField(form: FormGroup, field: string): boolean | null {
+        return form.controls[field].errors && form.controls[field].touched
+    }
+
+    public isFieldOneEqualFieldTwo(field1: string, field2: string) {
+        return (formGroup: AbstractControl): ValidationErrors | null => {
+            const fieldValue1 = formGroup.get(field1)?.value
+            const fieldValue2 = formGroup.get(field2)?.value
+
+            if (fieldValue1 !== fieldValue2) {
+                console.log({
+                    condicional: true,
+                    field1: fieldValue1,
+                    field2: fieldValue2,
+                })
+                formGroup.get(field2)?.setErrors({ notEqual: true })
+                return { notEqual: true }
+            }
+
+            formGroup.get(field2)?.setErrors(null)
+            return null
+        }
+    }
+}
